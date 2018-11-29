78b2804fb151e26116e0c96c6b425307a4206a57
Ana Karoline <anakromeiro@gmail.com>
2018-11-29 18:35:33 -0200

    First project in Angular 2

---
+++ src/app/first-component/first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-first-component',
+    //template inline
+    template: `
+    <p>My first component - Angular 2</p>
+    `
+}) //decorator
+export class FirstComponent{}
